ca4df7d3cf7b4ec3f36fe9c2f046e0818d760ce6
abdelrahmansweilam <abdelrahman_sweilam@yahoo.com>
2021-11-25 17:58:47 +0200

    AdjustState instead of removeHostageDamage

--- AI/src/code/Matrix.java
+++ AI/src/code/Matrix.java
@@ -8,9 +8,6 @@ import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.Stack;
 
-import jdk.internal.misc.FileSystemOption;
-
-
 public class Matrix extends SearchProblem {
 	String grid;
 	int nodesExp;
@@ -39,7 +36,7 @@ public class Matrix extends SearchProblem {
 		this.initialState+="0";//Neo's damage
 		//////System.out.println(initialState);
 		//Adding the initial state to generates states
-		generatedStates.add((this.initialState));
+		generatedStates.add(adjustState(this.initialState));
 		
 	}
 	@Override
@@ -407,16 +404,18 @@ public class Matrix extends SearchProblem {
 		}
 		return String.join(";", nextParsedState);
 	}
-	private static String removeHostageDamage(String state)
+	private static String adjustState(String state)
 	{
 		String[]parsedState=state.split(";");
-	
-		String[]hostageList=parsedState[7].split(",");
-	    String[]hostages=new String[hostageList.length/3];
-		for(int i=0;i<hostages.length;i++) {
-			hostages[i]=hostageList[i*3]+","+hostageList[i*3+1];
-		}
-		parsedState[7]=String.join(",", hostages);
+		parsedState[1] = "";
+		parsedState[3] = "";
+		parsedState[6] = "";
+		// String[]hostageList=parsedState[7].split(",");
+	    // String[]hostages=new String[hostageList.length/3];
+		// for(int i=0;i<hostages.length;i++) {
+		// 	hostages[i]=hostageList[i*3]+","+hostageList[i*3+1];
+		// }
+		// parsedState[7]=String.join(",", hostages);
 		return String.join(";", parsedState);
 	}
 	
@@ -458,9 +457,9 @@ public class Matrix extends SearchProblem {
         if (!allowExpandAction(node.operator, o))
             return null;
         String nextState = stateSpace(node.state, o);
-        if (nextState == null||this.generatedStates.contains((nextState)))
+        if (nextState == null||this.generatedStates.contains(adjustState(nextState)))
 			return null;
-        this.generatedStates.add((nextState));
+        this.generatedStates.add(adjustState(nextState));
         int addedCost = pathCost(o);
         if(((NeoActions)o) == NeoActions.KILL)
         {
@@ -749,7 +748,7 @@ public class Matrix extends SearchProblem {
 	private static MatrixSearchTreeNode IDS(MatrixSearchTreeNode root, Matrix m) {
 		for(int l=0;l<Integer.MAX_VALUE;l++) {
 			m.generatedStates=new HashSet<String>();
-			m.generatedStates.add((m.initialState));
+			m.generatedStates.add(adjustState(m.initialState));
 			Stack<MatrixSearchTreeNode> stack = new Stack<MatrixSearchTreeNode>();
 			List<NeoActions> operators = Arrays.asList(NeoActions.values());
 			stack.push(root);
@@ -792,7 +791,7 @@ public class Matrix extends SearchProblem {
 		//Matrix m=new Matrix(grid3);
 		////System.out.println(example);
 		////System.out.println("---------------------------------------------------------------");
-		System.out.println(solve(grid10,"DF",false));
+		System.out.println(solve(grid10,"BF",false));
 		////////System.out.println(m.stateSpace(m.initialState,NeoActions.TAKE_PILL));
 		////////System.out.println();
 		////////System.out.println("13,11;3;11,8;7,6;9,7,9,1,10,10,12,1;11,8,1,1;7,10,4,3,4,3,7,10,1,3,3,5,3,5,1,3,6,3,6,5,6,5,6,3,0,9,12,8,12,8,0,9,9,4,2,8,2,8,9,4,9,9,6,6,6,6,9,9,3,6,4,0,4,0,3,6,11,9,4,4,4,4,11,9,12,0,12,4,12,4,12,0,3,4,2,3,2,3,3,4,10,4,0,0,0,0,10,4,12,6,8,9,8,9,12,6,5,0,9,5,9,5,5,0,5,2,10,0,10,0,5,2,9,8,6,0,6,0,9,8,8,2,3,0,3,0,8,2,1,2,6,7,6,7,1,2,6,8,10,9,10,9,6,8,1,6,1,5,1,5,1,6,11,5,2,2,2,2,11,5,4,10,5,6,5,6,4,10,8,6,7,8,7,8,8,6,7,5,5,5,5,5,7,5,7,1,9,6,9,6,7,1,3,3,11,3,11,3,3,3,8,3,6,1,6,1,8,3,5,10,0,10,0,10,5,10,0,2,11,4,11,4,0,2,8,0,2,6,2,6,8,0,4,7,0,1,0,1,4,7,9,10,12,10,12,10,9,10,11,6,8,10,8,10,11,6,11,1,4,1,4,1,11,1,8,8,1,7,1,7,8,8,1,8,7,3,7,3,1,8,12,3,7,2,7,2,12,3,11,2,2,4,2,4,11,2,5,3,10,3,10,3,5,3,3,9,2,7,2,7,3,9,9,0,10,7,10,7,9,0,8,4,10,6,10,6,8,4,3,2,1,0,1,0,3,2,11,0,8,7,8,7,11,0,10,1,7,4,7,4,10,1,8,1,4,8,4,8,8,1,5,4,12,2,12,2,5,4,11,7,1,10,1,10,11,7,1,9,12,7,12,7,1,9,2,9,2,0,2,0,2,9,7,7,3,10,3,10,7,7,12,5,5,1,5,1,12,5,0,7,4,6,4,6,0,7,0,3,4,5,4,5,0,3,6,2,10,8,10,8,6,2,9,3,3,1,3,1,9,3,1,4,3,8,3,8,1,4,0,5,8,5,8,5,0,5;0,4,59,4,2,36,11,7,100;0,0,0;0");
